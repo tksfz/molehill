72e9d807ecc79f5bd8496a5fb3aa17a9d16dd824
thom <tom@tksfz.org>
2019-04-09 22:42:22 -0700

    Cleanup

--- src/main/scala/org/tksfz/molehill/aws/ec2.scala
+++ src/main/scala/org/tksfz/molehill/aws/ec2.scala
@@ -1,12 +1,10 @@
 package org.tksfz.molehill.aws
 
 import cats.data.Kleisli
-import cats.effect.IO
 import software.amazon.awssdk.services.ec2.Ec2AsyncClient
 
 object ec2 {
   type EC2Kleisli[F[_], A] = Kleisli[F, Ec2AsyncClient, A]
-  type EC2IO[A] = EC2Kleisli[IO, A]
 
   object EC2Kleisli {
     def apply[F[_], A](f: Ec2AsyncClient => F[A]): EC2Kleisli[F, A] = Kleisli(f)
