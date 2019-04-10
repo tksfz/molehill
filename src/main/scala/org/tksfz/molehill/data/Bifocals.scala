5190102969e16361fc442ccfad4956c40d5c2afb
thom <tom@tksfz.org>
2019-04-09 22:44:04 -0700

    Add ModifyBuilder

--- src/main/scala/org/tksfz/molehill/data/Bifocals.scala
+++ src/main/scala/org/tksfz/molehill/data/Bifocals.scala
@@ -8,6 +8,27 @@ import shapeless.{Lens, MkFieldLens, OpticDefns, Witness}
   */
 case class Bifocals[S1, S2, A](lens1: Lens[S1, A], lens2: Lens[S2, A]) {
   def >>(k: Witness)(implicit mkLens: MkFieldLens[A, k.T]): Bifocals[S1, S2, mkLens.Elem] = Bifocals(mkLens() compose this.lens1, mkLens() compose this.lens2)
+
+  def copyInto(s1: S1, s2: S2) = {
+    val a = lens1.get(s1)
+    lens2.set(s2)(a)
+  }
+}
+
+case class Quadfocals[S1, S2, A, T1, T2, B](lens1: Lens[S1, A], lens2: Lens[S2, A], lens3: Lens[T1, B], lens4: Lens[T2, B]) {
+  def >>(k: Witness)(implicit mkLens: MkFieldLens[A, k.T],
+                     mkLensT: MkFieldLens[B, k.T]): Quadfocals[S1, S2, mkLens.Elem, T1, T2, mkLensT.Elem] =
+    Quadfocals(mkLens() compose this.lens1, mkLens() compose this.lens2, mkLensT() compose this.lens3, mkLensT() compose this.lens4)
+
+  def copyFromTo(s1: S1, s2: S2) = {
+    val a = lens1.get(s1)
+    lens2.set(s2)(a)
+  }
+
+  def copyFromTo2(t1: T1, t2: T2) = {
+    val b = lens3.get(t1)
+    lens4.set(t2)(b)
+  }
 }
 
 object Bifocals {
@@ -17,3 +38,11 @@ object Bifocals {
     new Bifocals[C1, C2, A](OpticDefns.apply[C1] >> k, OpticDefns.apply[C2] >> k)
 }
 
+object Quadfocals {
+  def apply[C1, C2, A, D1, D2, B](k: Witness)
+                                 (implicit mkLensC1: MkFieldLens.Aux[C1, k.T, A],
+                                  mkLensC2: MkFieldLens.Aux[C2, k.T, A],
+                                  mkLensD1: MkFieldLens.Aux[D1, k.T, B],
+                                  mkLensD2: MkFieldLens.Aux[D2, k.T, B]) =
+    new Quadfocals[C1, C2, A, D1, D2, B](OpticDefns.apply[C1] >> k, OpticDefns.apply[C2] >> k, OpticDefns.apply[D1] >> k, OpticDefns.apply[D2] >> k)
+}
--- src/main/scala/org/tksfz/molehill/plan/Plan.scala
+++ src/main/scala/org/tksfz/molehill/plan/Plan.scala
@@ -5,16 +5,72 @@ import java.util.concurrent.CompletableFuture
 import cats.Id
 import cats.data.Kleisli
 import cats.effect.Async
-import cats.free.Free
 import monix.catnap.FutureLift
-import org.tksfz.molehill.aws.ec2.EC2Kleisli
-import org.tksfz.molehill.data.Predicted
+import org.tksfz.molehill.aws.ec2.{EC2Kleisli}
+import org.tksfz.molehill.data.{Predicted, Quadfocals, SequencePredicted}
 import software.amazon.awssdk.services.ec2.Ec2AsyncClient
+
+import scala.language.higherKinds
 
 sealed trait Plan[A]
 
 case class CreateAnew[Spec[_[_]], Exports[_[_]]](postSpec: Spec[Predicted], postExports: Exports[Predicted],
                                                  execute: EC2Kleisli[PlanIO, Exports[Id]]) extends Plan[Exports[Predicted]]
+
+case class ModifyBuilder[Spec[_[_]], Exports[_[_]]](preSpec: Spec[Id], predictedExports: Exports[Predicted], targetSpec: Spec[Predicted],
+                                                    execute: Kleisli[PlanIO, (Ec2AsyncClient, Spec[Id]), Exports[Id]])
+                                                   (implicit sequencer: SequencePredicted[Spec]) {
+  lazy val preSpecLocal: Spec[Predicted] = ???
+
+  // TODO: handle deferreds
+  def updated(updatePredictedExports: Exports[Predicted] => Exports[Predicted])(next: Exports[Id] => Kleisli[PlanIO, (Ec2AsyncClient, Spec[Id]), Exports[Id]]) = {
+    ModifyBuilder(preSpec, updatePredictedExports(predictedExports), targetSpec, execute.flatMap(next))
+  }
+
+  def withFieldSolver[A](field: Quadfocals[Spec[Predicted], Exports[Predicted], Predicted[A], Spec[Id], Exports[Id], Id[A]],
+                         f: Exports[Id] => Kleisli[PlanIO, (Ec2AsyncClient, Spec[Id]), A]): ModifyBuilder[Spec, Exports] = {
+    if (isInconsistent(field.lens1.get, preSpecLocal, targetSpec)) {
+      updated(field.copyFromTo(targetSpec, _)) {
+        exports =>
+          f(exports).map { a =>
+            field.lens4.set(exports)(a)
+          }
+      }
+    } else {
+      this
+    }
+  }
+
+  /** Determines whether the attribute specified by f is inconsistent between the targetSpec and preSpec.
+    *
+    * Should this require f: Spec[D] => D[A]?
+    *
+    * @param f selects an attribute from the specification type Spec
+    * @return true when the value of the attribute in the preSpec is inconsistent with the targetSpec
+    */
+  private def isInconsistent[Spec[_[_]], A, Exports[_[_]]](f: Spec[Predicted] => A, preSpec: Spec[Predicted], targetSpec: Spec[Predicted]): Boolean = {
+    val a = f(preSpec)
+    val b = f(targetSpec)
+    a != b
+  }
+
+  def toEC2PlanKleisli: EC2Kleisli[PlanIO, Exports[Id]] = {
+    Kleisli[PlanIO, Ec2AsyncClient, Exports[Id]] { ec2 =>
+      for {
+        spec <- sequencer(targetSpec)
+        r <- execute.run(ec2, spec)
+      } yield r
+    }
+  }
+}
+
+object ModifyBuilder {
+  def apply[Spec[_[_]] : SequencePredicted, Exports[_[_]]](preSpec: Spec[Id], preExports: Exports[Id],
+                                                           targetSpec: Spec[Predicted]): ModifyBuilder[Spec, Exports] = {
+    val preExportsLocal: Exports[Predicted] = ???
+    ModifyBuilder(preSpec, preExportsLocal, targetSpec, Kleisli.pure[PlanIO, (Ec2AsyncClient, Spec[Id]), Exports[Id]](preExports))
+  }
+}
 
 case class Modify[Spec[_[_]], Exports[_[_]]](preSpec: Spec[Id], postSpec: Spec[Predicted], postExports: Exports[Predicted],
                                              execute: EC2Kleisli[PlanIO, Exports[Id]]) extends Plan[Exports[Predicted]]
